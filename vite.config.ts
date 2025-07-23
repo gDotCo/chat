66df3758c21a7a1eded8ff0a692cea98623ae95e
anil-g11h <anilraj659@gmail.com>
2025-07-24 00:46:15 +0530

    fix key

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: env.BASE_PATH,
-    define: {
-      'process.env.VITE_ABLY_KEY': JSON.stringify(env.VITE_ABLY_KEY)
-    },
+    // define: {
+    //   'process.env.VITE_ABLY_KEY': JSON.stringify(env.VITE_ABLY_KEY)
+    // },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
